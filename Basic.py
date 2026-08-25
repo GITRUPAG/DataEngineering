41b7e0fb387d9cca5ba4f6ec962c934526c74c17
GITRUPAG <rupag12004@gmail.com>
2026-08-25 21:14:48 +0530

    first commit

---
+++ Basic.py
@@ -0,0 +1,42 @@
+print("Hello World!!")
+
+# Cpython -> bytecode -> executed by
+# python virtual machine
+
+name = 'alice'
+age = 25
+salary = 258.233
+
+student_name = "Bob"
+StudentName = "BobNEW"
+
+# print(type(age)) - Data type of varibale
+
+price = 222.99
+
+is_active = False
+
+# Type coversion
+Age = "25"
+
+age = int(Age)
+print(type(age))
+
+# int(), float(), str(), bool()
+
+Salary = "20.252"
+salary = float(Salary)
+
+#
+
+# 1salary
+# emp-name
+x, y, z = 10, 20, 30
+
+a = b = c = 100
+
+
+
+
+
+
