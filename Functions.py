ad97df361d9e74317fdfa070edc0bd1454dc58c1
GITRUPAG <rupag12004@gmail.com>
2026-09-03 11:50:11 +0530

    Python Functions

---
+++ Functions.py
@@ -0,0 +1,96 @@
+# Python Functions
+
+price = 5000
+quantity = 5
+total = price * quantity
+print(total)
+
+# def function_name():
+#     statements
+
+# print("Hello")
+
+def greet():
+    print("Hello")
+
+# print(greet() + "World!!")
+
+def greet_name(name): # parameters
+    print("Hello", name)
+
+greet_name("Ravi") # arguments
+
+def calculate_total(price, quantity):
+    total = price * quantity
+    print(total)
+
+calculate_total(1020, 5)
+calculate_total(100, 5)
+
+def add(a, b):
+    return a + b # sends the value back, reuse the result
+
+print("total : ", add(1,2) + 3)
+
+# greet_name()
+
+def greet_name1(name, message="Hello"):
+    print(message, name)
+
+greet_name1("Tharun", "Good Morning!!")
+
+def employee(name, salary):
+    print(name, salary)
+
+employee(salary=50000, name="Ravi")
+
+def is_valid(amount, status):
+    return amount > 0 and status =="SUCCESS"
+
+print(is_valid(0, "PENDING"))
+
+employees = [
+    {"name": "ravi", "salary": 4000},
+{"name": "rahul", "salary": 500},
+{"name": "priya", "salary": 2000},
+{"name": "john", "salary": 3000},
+]
+
+def clean_salary(salary):
+    return float(salary)
+
+for employee in employees:
+    employee["salary"] = clean_salary(employee["salary"])
+
+print(employees)
+
+amount = 5000 # global variable
+def calculate():
+    amount = 1000 # local variable
+
+print(amount)
+
+calculate()
+
+def calculate_new(a, b):
+    total = a + b
+    diff = a - b
+    return total, diff
+
+total, diff = calculate_new(10, 5)
+print(total)
+print(diff)
+
+def calculate_total(numbers):
+    return sum(numbers)
+
+numbers = [1, 2 , 3 , 4 ]
+print("Total : " , calculate_total(numbers))
+
+
+def high_salary(employees):
+    return [
+        employee for employee in employees if employee["salary"] > 500
+    ]
+
+print(high_salary(employees))
